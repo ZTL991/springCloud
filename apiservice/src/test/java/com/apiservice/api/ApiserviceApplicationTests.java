dbe679a141e7568e8b509bf8d9974ca43183273f
ztl <3090757365@qq.com>
2020-07-02 14:52:29 +0800

    zuul

---
+++ apiservice/src/test/java/com/apiservice/api/ApiserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apiservice.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
