1179441cb6f36643aa5033c8eac8963b9014ea88
ztl <3090757365@qq.com>
2020-07-02 14:51:30 +0800

    zuul

---
+++ apiservice/src/main/java/com/apiservice/api/ApiserviceApplication.java
@@ -0,0 +1,17 @@
+package com.apiservice.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableZuulProxy
+public class ApiserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiserviceApplication.class, args);
+    }
+
+}
